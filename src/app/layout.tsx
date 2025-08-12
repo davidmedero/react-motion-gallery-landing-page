78f967865cb4d1936f096bc065ddb855966f3da9
David Medero <david.gregory.medero@gmail.com>
2025-08-11 23:05:53 -0400

    testing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Nav from "./components/Nav";
-import styles from './page.module.css'
 import { OverlayProvider } from "./contexts/OverlayContext";
 
 const geistSans = Geist({
