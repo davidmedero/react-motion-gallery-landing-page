3ddd26108098ee09408b0a32beb6bf1897e2add8
David Medero <david.gregory.medero@gmail.com>
2025-08-12 02:51:32 -0400

    cleaning up

--- src/app/lib/scrollLock.ts
+++ src/app/lib/scrollLock.ts
@@ -1,32 +1,15 @@
-// lib/scrollLock.ts
 let locks = 0;
-let savedY = 0;
 
 function apply() {
-  savedY = window.scrollY || window.pageYOffset;
-  const sbw = window.innerWidth - document.documentElement.clientWidth;
   const b = document.body;
-  b.style.position = 'fixed';
-  b.style.top = `-${savedY}px`;
-  b.style.left = '0';
-  b.style.right = '0';
-  b.style.width = '100%';
   b.style.overflow = 'hidden';
-  if (sbw > 0) b.style.paddingRight = `${sbw}px`;
   b.style.touchAction = 'none';
 }
 
 function remove() {
   const b = document.body;
-  b.style.position = '';
-  b.style.top = '';
-  b.style.left = '';
-  b.style.right = '';
-  b.style.width = '';
   b.style.overflow = '';
-  b.style.paddingRight = '';
   b.style.touchAction = '';
-  window.scrollTo(0, savedY);
 }
 
 export function lockBody() {
